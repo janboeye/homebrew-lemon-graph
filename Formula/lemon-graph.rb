3353ea4cb3ddf18780e6a507cf36c5c00b99218a
Vitor Bandeira <vvbandeira@baldr.local>
2023-06-22 20:39:32 -0300

    Use our own GH fork due to network issues
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- Formula/lemon-graph.rb
+++ Formula/lemon-graph.rb
@@ -1,9 +1,9 @@
 class LemonGraph < Formula
     desc "Library for Efficient Modeling and Optimization in Networks"
     homepage "https://lemon.cs.elte.hu/trac/lemon"
-    sha256 "71b7c725f4c0b4a8ccb92eb87b208701586cf7a96156ebd821ca3ed855bad3c8"
-    head "https://github.com/tpet/lemon"
-    url "http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz"
+    sha256 "ff00cc1b7aafd8481bf8a1b13303de79ec4dec485e41c807f9e95f369b917d78"
+    head "https://github.com/The-OpenROAD-Project/lemon-graph"
+    url "https://github.com/The-OpenROAD-Project/lemon-graph/archive/refs/tags/1.3.1.tar.gz"
     version "1.3.1"
     license "BSL-1.0"
 
